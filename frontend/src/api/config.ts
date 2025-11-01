2a4ff66f01af39cc9c56edb25cf962a186d7a3da
Nitin Rajput <nitinrajput0093@gmail.com>
2025-11-01 07:40:41 +0530

    fix:fixed some config  for frontend deployment

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,7 +1,9 @@
+
 import axios from "axios";
 
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BASE_URL  ||"http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
